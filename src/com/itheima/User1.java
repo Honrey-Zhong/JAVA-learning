fe21d58752bd51c38f90fca2df00c72feb6ac277
Honrey-Zhong <1304560647@qq.com>
2025-05-15 12:23:18 +0800

    添加文件

---
+++ src/com/itheima/User1.java
@@ -0,0 +1,20 @@
+package com.itheima;
+
+public class User1 {
+	private int id;
+	private String name;
+	private String password;
+	//有参构造
+	public User1(int id, String name, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "User1 [id=" + id + ", name=" + name + ", password=" + password + "]";
+	}
+	
+
+}
